0dc402e210e7b6892ee5f48df6531ef7f623e3f6
Romina <romina.khanmohammadi99@gmail.com>
2024-01-15 17:06:54 +0330

    fix shortest path

--- matrixes.cpp
+++ matrixes.cpp
@@ -1,4 +1,6 @@
 #include "matrixes.hpp"
+#include <string>
+
 using namespace  std;
 
 
@@ -64,62 +66,62 @@ matrixes::matrixes()
     names.insert({"Kouhsar", 57});
     names.insert({"Chaharbagh", 58}); 
 
-    distance[0][10].setinfo(10, "metro_taxi");  distance[10][0].setinfo(10, "metro_taxi");
-    distance[10][11].setinfo(6, "metro_taxi");  distance[11][10].setinfo(6, "metro_taxi");  
-    distance[11][12].setinfo(2, "metro_taxi");  distance[12][11].setinfo(2, "metro_taxi");
-    distance[11][16].setinfo(2, "metro_taxi");  distance[16][11].setinfo(2, "metro_taxi");
-    distance[12][25].setinfo(5, "metro_taxi");  distance[25][12].setinfo(2, "metro_taxi");
-    distance[25][43].setinfo(2, "metro_taxi");  distance[43][25].setinfo(2, "metro_taxi");
-    distance[43][48].setinfo(2, "metro_taxi");  distance[48][43].setinfo(2, "metro_taxi");
-    distance[48][50].setinfo(8, "metro_taxi");  distance[50][48].setinfo(8, "metro_taxi");
-    distance[50][55].setinfo(6, "metro_taxi");  distance[55][50].setinfo(6, "metro_taxi");
-    distance[55][57].setinfo(10, "metro_taxi"); distance[57][55].setinfo(10, "metro_taxi");
-    distance[55][58].setinfo(5, "metro_taxi");  distance[58][55].setinfo(5, "metro_taxi");
-    distance[55][54].setinfo(4, "metro_taxi");  distance[54][55].setinfo(4, "metro_taxi");
-    distance[54][53].setinfo(5, "metro_taxi");  distance[53][54].setinfo(5, "metro_taxi");
-    distance[53][52].setinfo(6, "metro_taxi");  distance[52][53].setinfo(6, "metro_taxi");
-    distance[52][51].setinfo(2, "metro_taxi");  distance[51][52].setinfo(2, "metro_taxi");
-    distance[51][49].setinfo(4, "metro_taxi");  distance[49][51].setinfo(4, "metro_taxi");
-    distance[49][47].setinfo(2, "metro_taxi");  distance[47][49].setinfo(2, "metro_taxi");
-    distance[47][46].setinfo(1, "metro_taxi");  distance[46][47].setinfo(1, "metro_taxi");
-    distance[46][42].setinfo(3, "metro_taxi");  distance[42][46].setinfo(3, "metro_taxi");
-    distance[42][41].setinfo(4, "metro_taxi");  distance[41][42].setinfo(4, "metro_taxi");
-    distance[41][40].setinfo(3, "metro_taxi");  distance[40][41].setinfo(3, "metro_taxi");
-    distance[40][39].setinfo(1, "metro_taxi");  distance[39][40].setinfo(1, "metro_taxi");
-    distance[39][38].setinfo(2, "metro_taxi");  distance[38][39].setinfo(2, "metro_taxi");
-    distance[38][37].setinfo(2, "metro_taxi");  distance[37][38].setinfo(2, "metro_taxi");
-    distance[37][36].setinfo(8, "metro_taxi");  distance[36][37].setinfo(8, "metro_taxi");
-    distance[42][43].setinfo(2, "metro_taxi");  distance[43][42].setinfo(2, "metro_taxi");
-    distance[43][44].setinfo(1, "metro_taxi");  distance[44][43].setinfo(1, "metro_taxi");
-    distance[44][34].setinfo(2, "metro_taxi");  distance[34][44].setinfo(2, "metro_taxi");
-    distance[34][27].setinfo(3, "metro_taxi");  distance[27][34].setinfo(3, "metro_taxi");
-    distance[27][28].setinfo(7, "metro_taxi");  distance[28][27].setinfo(7, "metro_taxi");
-    distance[28][29].setinfo(5, "metro_taxi");  distance[29][28].setinfo(5, "metro_taxi");
-    distance[29][30].setinfo(4, "metro_taxi");  distance[30][29].setinfo(4, "metro_taxi");
-    distance[30][31].setinfo(3, "metro_taxi");  distance[31][30].setinfo(3, "metro_taxi");
-    distance[31][32].setinfo(2, "metro_taxi");  distance[32][31].setinfo(2, "metro_taxi");
-    distance[27][17].setinfo(1, "metro_taxi");  distance[17][27].setinfo(1, "metro_taxi");
-    distance[17][13].setinfo(2, "metro_taxi");  distance[13][17].setinfo(2, "metro_taxi");
-    distance[13][8].setinfo(5, "metro_taxi");   distance[8][13].setinfo(5, "metro_taxi");
-    distance[8][6].setinfo(10, "metro_taxi");   distance[6][8].setinfo(10, "metro_taxi");
-    distance[3][6].setinfo(4, "metro_taxi");    distance[6][3].setinfo(4, "metro_taxi");
-    distance[26][27].setinfo(2, "metro_taxi");  distance[27][26].setinfo(2, "metro_taxi");
-    distance[24][25].setinfo(2, "metro_taxi");  distance[25][24].setinfo(2, "metro_taxi");
-    distance[24][23].setinfo(1, "metro_taxi");  distance[23][24].setinfo(1, "metro_taxi");
-    distance[22][23].setinfo(4, "metro_taxi");  distance[23][22].setinfo(4, "metro_taxi");
-    distance[21][22].setinfo(6, "metro_taxi");  distance[22][21].setinfo(6, "metro_taxi");
-    distance[20][21].setinfo(3, "metro_taxi");  distance[21][20].setinfo(3, "metro_taxi");
-    distance[20][19].setinfo(9, "metro_taxi");  distance[19][20].setinfo(9, "metro_taxi");
-    distance[19][18].setinfo(13, "metro_taxi"); distance[18][19].setinfo(13, "metro_taxi");
-    distance[33][42].setinfo(1, "metro_taxi");  distance[42][33].setinfo(1, "metro_taxi");
-    distance[33][23].setinfo(1, "metro_taxi");  distance[23][33].setinfo(1, "metro_taxi");
-    distance[16][23].setinfo(2, "metro_taxi");  distance[23][16].setinfo(2, "metro_taxi");
-    distance[9][11].setinfo(1, "metro_taxi");   distance[11][9].setinfo(1, "metro_taxi");
-    distance[7][9].setinfo(3, "metro_taxi");    distance[9][7].setinfo(3, "metro_taxi");
-    distance[7][5].setinfo(1, "metro_taxi");    distance[5][7].setinfo(1, "metro_taxi");
-    distance[5][4].setinfo(1, "metro_taxi");    distance[4][5].setinfo(1, "metro_taxi");
-    distance[4][2].setinfo(7, "metro_taxi");    distance[2][4].setinfo(7, "metro_taxi");
-    distance[25][26].setinfo(2, "metro_taxi");  distance[26][25].setinfo(2, "metro_taxi");
+    distance[0][10].setinfo(10, "Metro_Taxi");  distance[10][0].setinfo(10, "Metro_Taxi");
+    distance[10][11].setinfo(6, "Metro_Taxi");  distance[11][10].setinfo(6, "Metro_Taxi");  
+    distance[11][12].setinfo(2, "Metro_Taxi");  distance[12][11].setinfo(2, "Metro_Taxi");
+    distance[11][16].setinfo(2, "Metro_Taxi");  distance[16][11].setinfo(2, "Metro_Taxi");
+    distance[12][25].setinfo(5, "Metro_Taxi");  distance[25][12].setinfo(5, "Metro_Taxi");
+    distance[25][43].setinfo(2, "Metro_Taxi");  distance[43][25].setinfo(2, "Metro_Taxi");
+    distance[43][48].setinfo(2, "Metro_Taxi");  distance[48][43].setinfo(2, "Metro_Taxi");
+    distance[48][50].setinfo(8, "Metro_Taxi");  distance[50][48].setinfo(8, "Metro_Taxi");
+    distance[50][55].setinfo(6, "Metro_Taxi");  distance[55][50].setinfo(6, "Metro_Taxi");
+    distance[55][57].setinfo(10, "Metro_Taxi"); distance[57][55].setinfo(10, "Metro_Taxi");
+    distance[55][58].setinfo(5, "Metro_Taxi");  distance[58][55].setinfo(5, "Metro_Taxi");
+    distance[55][54].setinfo(4, "Metro_Taxi");  distance[54][55].setinfo(4, "Metro_Taxi");
+    distance[54][53].setinfo(5, "Metro_Taxi");  distance[53][54].setinfo(5, "Metro_Taxi");
+    distance[53][52].setinfo(6, "Metro_Taxi");  distance[52][53].setinfo(6, "Metro_Taxi");
+    distance[52][51].setinfo(2, "Metro_Taxi");  distance[51][52].setinfo(2, "Metro_Taxi");
+    distance[51][49].setinfo(4, "Metro_Taxi");  distance[49][51].setinfo(4, "Metro_Taxi");
+    distance[49][47].setinfo(2, "Metro_Taxi");  distance[47][49].setinfo(2, "Metro_Taxi");
+    distance[47][46].setinfo(1, "Metro_Taxi");  distance[46][47].setinfo(1, "Metro_Taxi");
+    distance[46][42].setinfo(3, "Metro_Taxi");  distance[42][46].setinfo(3, "Metro_Taxi");
+    distance[42][41].setinfo(4, "Metro_Taxi");  distance[41][42].setinfo(4, "Metro_Taxi");
+    distance[41][40].setinfo(3, "Metro_Taxi");  distance[40][41].setinfo(3, "Metro_Taxi");
+    distance[40][39].setinfo(1, "Metro_Taxi");  distance[39][40].setinfo(1, "Metro_Taxi");
+    distance[39][38].setinfo(2, "Metro_Taxi");  distance[38][39].setinfo(2, "Metro_Taxi");
+    distance[38][37].setinfo(2, "Metro_Taxi");  distance[37][38].setinfo(2, "Metro_Taxi");
+    distance[37][36].setinfo(8, "Metro_Taxi");  distance[36][37].setinfo(8, "Metro_Taxi");
+    distance[42][43].setinfo(2, "Metro_Taxi");  distance[43][42].setinfo(2, "Metro_Taxi");
+    distance[43][44].setinfo(1, "Metro_Taxi");  distance[44][43].setinfo(1, "Metro_Taxi");
+    distance[44][34].setinfo(2, "Metro_Taxi");  distance[34][44].setinfo(2, "Metro_Taxi");
+    distance[34][27].setinfo(3, "Metro_Taxi");  distance[27][34].setinfo(3, "Metro_Taxi");
+    distance[27][28].setinfo(7, "Metro_Taxi");  distance[28][27].setinfo(7, "Metro_Taxi");
+    distance[28][29].setinfo(5, "Metro_Taxi");  distance[29][28].setinfo(5, "Metro_Taxi");
+    distance[29][30].setinfo(4, "Metro_Taxi");  distance[30][29].setinfo(4, "Metro_Taxi");
+    distance[30][31].setinfo(3, "Metro_Taxi");  distance[31][30].setinfo(3, "Metro_Taxi");
+    distance[31][32].setinfo(2, "Metro_Taxi");  distance[32][31].setinfo(2, "Metro_Taxi");
+    distance[27][17].setinfo(1, "Metro_Taxi");  distance[17][27].setinfo(1, "Metro_Taxi");
+    distance[17][13].setinfo(2, "Metro_Taxi");  distance[13][17].setinfo(2, "Metro_Taxi");
+    distance[13][8].setinfo(5, "Metro_Taxi");   distance[8][13].setinfo(5, "Metro_Taxi");
+    distance[8][6].setinfo(10, "Metro_Taxi");   distance[6][8].setinfo(10, "Metro_Taxi");
+    distance[3][6].setinfo(4, "Metro_Taxi");    distance[6][3].setinfo(4, "Metro_Taxi");
+    distance[26][27].setinfo(2, "Metro_Taxi");  distance[27][26].setinfo(2, "Metro_Taxi");
+    distance[24][25].setinfo(2, "Metro_Taxi");  distance[25][24].setinfo(2, "Metro_Taxi");
+    distance[24][23].setinfo(1, "Metro_Taxi");  distance[23][24].setinfo(1, "Metro_Taxi");
+    distance[22][23].setinfo(4, "Metro_Taxi");  distance[23][22].setinfo(4, "Metro_Taxi");
+    distance[21][22].setinfo(6, "Metro_Taxi");  distance[22][21].setinfo(6, "Metro_Taxi");
+    distance[20][21].setinfo(3, "Metro_Taxi");  distance[21][20].setinfo(3, "Metro_Taxi");
+    distance[20][19].setinfo(9, "Metro_Taxi");  distance[19][20].setinfo(9, "Metro_Taxi");
+    distance[19][18].setinfo(13, "Metro_Taxi"); distance[18][19].setinfo(13, "Metro_Taxi");
+    distance[33][42].setinfo(1, "Metro_Taxi");  distance[42][33].setinfo(1, "Metro_Taxi");
+    distance[33][23].setinfo(1, "Metro_Taxi");  distance[23][33].setinfo(1, "Metro_Taxi");
+    distance[16][23].setinfo(2, "Metro_Taxi");  distance[23][16].setinfo(2, "Metro_Taxi");
+    distance[9][11].setinfo(1, "Metro_Taxi");   distance[11][9].setinfo(1, "Metro_Taxi");
+    distance[7][9].setinfo(3, "Metro_Taxi");    distance[9][7].setinfo(3, "Metro_Taxi");
+    distance[7][5].setinfo(1, "Metro_Taxi");    distance[5][7].setinfo(1, "Metro_Taxi");
+    distance[5][4].setinfo(1, "Metro_Taxi");    distance[4][5].setinfo(1, "Metro_Taxi");
+    distance[4][2].setinfo(7, "Metro_Taxi");    distance[2][4].setinfo(7, "Metro_Taxi");
+    distance[25][26].setinfo(2, "Metro_Taxi");  distance[26][25].setinfo(2, "Metro_Taxi");
     
     distance[1][9].setinfo(10, "Bus");  distance[9][1].setinfo(10, "Bus");
     distance[9][12].setinfo(4, "Bus");  distance[12][9].setinfo(4, "Bus");
@@ -141,45 +143,66 @@ matrixes::matrixes()
 
 }
 
-void matrixes::search_in_map(int i, int j)
+string matrixes::search_in_map(int i)
 {
     for(auto item:names)
     {
         if(item.second == i)
         {
-            cout << item.first;
-            break;
+            return item.first;
         }
     }
-
-    cout << "---->";
-
-    for(auto item:names)
-    {
-        if(item.second == j)
-        {
-            cout << item.first;
-            break;
-        }
-    } 
-
-    cout << '\n';
 }
 
-void matrixes:: print()
+int matrixes::minDistance(node dist[], bool sptSet[])
 {
-    for(int i = 0 ; i <= 58 ; i++)
-    {
-        for(int j = 0 ; j <= 58 ; j++)
-        {   
-            if(distance[i][j].getdis() != 0)
-            {
-                search_in_map(i,j);
-                cout << distance[i][j].getdis() << " ";
-                cout << distance[i][j].getvic();
-            }
-        }
+ 
+    int min = INT_MAX, min_index;
+ 
+    for (int i = 0; i < 59; i++)
+        if (sptSet[i] == false && dist[i].dist <= min)
+            min = dist[i].dist, min_index = i;
+ 
+    return min_index;
+}
+void matrixes::run()
+{
+    string start, end;
+    getline(cin, start);
+    getline(cin, end);
 
-        cout << endl;
+    find_short_path(names[start], names[end]);
+}
+
+void matrixes::find_short_path(int start, int end)
+{
+    node shortest[59]; 
+    bool sptSet[59]; 
+  
+    for (int i = 0; i < 59; i++)
+        shortest[i].dist = INT_MAX, sptSet[i] = false;
+ 
+    
+    shortest[start].dist = 0;
+    shortest[start].direction = search_in_map(start);
+ 
+    for (int count = 0; count < 58; count++) {
+      
+        int u = minDistance(shortest, sptSet);
+ 
+        sptSet[u] = true;
+ 
+        for (int v = 0; v < 59; v++)
+ 
+            if (!sptSet[v] && distance[u][v].getdis()
+                && shortest[u].dist != INT_MAX
+                && shortest[u].dist + distance[u][v].getdis() < shortest[v].dist)
+                {
+                    shortest[v].dist = shortest[u].dist + distance[u][v].getdis();
+                    shortest[v].direction = shortest[u].direction + " -> " + search_in_map(v);
+                }
     }
+ 
+    cout << shortest[end].dist << '\n';
+    cout << shortest[end].direction;
 }
--- matrixes.hpp
+++ matrixes.hpp
@@ -3,11 +3,19 @@
 
 #include <iostream>
 #include <map>
+#include <limits.h>
+
 
 #include "station.hpp"
 #include "matrixes.hpp"
 
 using namespace std;
+
+struct node
+{
+    int dist = INT_MAX;
+    string direction = "";
+};
 
 class matrixes
 {
@@ -18,7 +26,10 @@ class matrixes
     public:
     matrixes();
     void print();
-    void search_in_map(int, int); 
+    string search_in_map(int);
+    void find_short_path(int start, int end);
+    int minDistance(node dist[], bool sptSet[]); 
+    void run();
 };
 
 #endif
